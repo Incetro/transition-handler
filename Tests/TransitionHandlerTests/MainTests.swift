d8377eccc18916354195440feee7f24376c30e33
Dmitry <savinov.dsta@gmail.com>
2020-11-25 16:27:32 +0300

    Basic Pod structure.

---
+++ Tests/TransitionHandlerTests/MainTests.swift
@@ -0,0 +1,13 @@
+//
+//  MainTests.swift
+//  TransitionHandler
+//
+//  Created by Дмитрий Савинов on 23.11.2020.
+//
+
+import XCTest
+import TransitionHandler
+
+class MainTest: XCTestCase {
+}
+
