0c718cee6c3d5404ea577e86215c0a5427efe205
Alexander Lezya <>
2021-01-29 19:44:48 +0300

    Intermediate commit

--- ExampleTransitionHandler/Pods/Target Support Files/Pods-ExampleTransitionHandler/Pods-ExampleTransitionHandler-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ExampleTransitionHandler : NSObject
-@end
-@implementation PodsDummy_Pods_ExampleTransitionHandler
-@end
--- ExampleTransitionHandler/Pods/Target Support Files/TransitionHandler/TransitionHandler-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TransitionHandler : NSObject
-@end
-@implementation PodsDummy_TransitionHandler
-@end
