f50f716b3c5d89f360f745cf1074d2b0d9c19200
TraxxasWyls <savinov.dsta@gmail.com>
2023-11-01 18:14:04 +0300

    Add package support

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "transition-handler",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TransitionHandler",
+            targets: ["TransitionHandler"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TransitionHandler"),
+        .testTarget(
+            name: "TransitionHandlerTests",
+            dependencies: ["TransitionHandler"]),
+    ]
+)
